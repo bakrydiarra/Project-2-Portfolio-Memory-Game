0874680b3fc07b910c4d36b7f93c5871ab4f27a9
BakryDiarra <bakrydiarra@gmail.com>
2023-01-19 17:00:57 +0000

    Change overlay class = won, lost, and rules. Set the popup modal for button rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 const cards = document.querySelectorAll(".memory-card");
+const trigger = document.getElementById("trigger");
+const rules = document.getElementById("rules");
+const  closeRules = document.getElementById("close-rules");
 
 function flipCard() {
     this.classList.add("flip")
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+function showRules() {
+    rules.style.display = "block";
+}
+
+trigger.addEventListener("click", showRules);
+
+function exitRules () {
+    rules.style.display = "none";
+}
+
+closeRules.addEventListener("click", exitRules);
