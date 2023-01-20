0983c2dadb7c9e35e24b06ad17eb99892d4da323
BakryDiarra <bakrydiarra@gmail.com>
2023-01-20 23:49:33 +0000

    Add function startCountdown that sets the timer and add function gameOver that trigger that  modal window lost

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 const cards = document.querySelectorAll(".memory-card");
 const trigger = document.getElementById("trigger");
 const rules = document.getElementById("rules");
-const  closeRules = document.getElementById("close-rules");
+const closeRules = document.getElementById("close-rules");
+const lost = document.getElementById("lost");
 let cardOne, cardTwo;
 let boardFreeze = false;
 let flipped = false;
+let StartGame = false;
 let matched;
-let win;
-let lost;
 let timer = 60;
+let moves = 1;
 let reset;
 
 /* event */
@@ -22,19 +23,27 @@ closeRules.addEventListener("click", exitRules);
 codde take from https://www.youtube.com/playlist?list=PLngoRLGHq3kCpoT0urRHDPsNVTM7aYsiv and adapted */
 
 function flipCard() {
-if (boardFreeze) return;  /* return is boardFreeze is true then the rest of the function wont be executed */
-if (this === cardOne) return; /* to identify the first card flipped */
-
-this.classList.add("flip");
-
-if (!flipped) {
-    flipped = true;
-    cardOne = this;  /* fixed the cardOne identitiy */
-    return;
+    if (!StartGame) {
+        StartGame = true;
+        startCountdown(60);
+    }
     
-}
-cardTwo = this; /* identify the cardTwo */
-cardsMatchCheck ();
+    if (boardFreeze) return; /* return is boardFreeze is true then the rest of the function wont be executed */
+    if (this === cardOne) return; /* to identify the first card flipped */
+    
+    this.classList.add("flip");
+
+    
+
+    if (!flipped) {
+        flipped = true;
+        cardOne = this; /* fixed the cardOne identitiy */
+        return;
+
+
+    }
+    cardTwo = this; /* identify the cardTwo */
+    cardsMatchCheck();
 }
 
 function showRules() {
@@ -42,35 +51,93 @@ function showRules() {
 }
 
 
-function exitRules () {
+function exitRules() {
     rules.style.display = "none";
 }
 
-function cardsMatchCheck () {
+function cardsMatchCheck() {
     matched = cardOne.dataset.name === cardTwo.dataset.name;
 
     if (matched) {
-        unFlipCard();
+        lockedCards();
     } else {
-        notmached ();
+        notmached();
     }
 }
 
-function unFlipCard () {
+function lockedCards() {
 
     cardOne.removeEventListener('click', flipCard);
     cardTwo.removeEventListener('click', flipCard);
-    recountCards ();
+    recountCards();
 }
 
-function notmached () {
-    cardOne.classList.remove("flip");
-    cardTwo.classList.remove("flip");
+function notmached() {
+    boardFreeze = true;
+    setTimeout(() => {
+        cardOne.classList.remove("flip");
+        cardTwo.classList.remove("flip");
+
+        recountCards();
+    }, 900);
+
+    addmoves();
 }
 
-function recountCards () {
+
+
+function recountCards() {
     boardFreeze = false;
     flipped = false;
     cardOne = null;
-    cardTwo =  null;
+    cardTwo = null;
+
 }
+
+/* add moves after a second so that it appears after the remove("flip") */
+
+function addmoves() {
+    setTimeout(() => {
+        document.getElementById("moves").innerHTML = moves++;
+    }, 1000);
+}
+
+
+/* function gameOver() {
+    lost.style.display = "block";
+} */
+
+/* function CountDown() {
+    setInterval(() => {
+        timer--;
+        document.getElementById("timer").innerHTML = timer;
+        if (timer == 1) {
+            clearInterval(timer);
+
+        }
+    }, 1000);
+
+} */
+
+
+function startCountdown(seconds) {
+    let counter = seconds;
+      
+    const interval = setInterval(() => {
+     counter--;
+        document.getElementById("timer").innerHTML = counter;
+      if (counter < 1 ) {
+        clearInterval(interval);
+        gameOver ();
+      }
+    }, 1000);
+    
+  }
+  
+  function gameOver () {
+
+    lost.style.display = "block";
+  }
+   
+  
+  
